0d0094c8a26e9313c01e538f853aa4bd93d9f8ac
zlaa <72907523+zlaazlaa@users.noreply.github.com>
2025-01-17 23:45:06 +0800

    Update WelcomePage.vue

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -6,17 +6,6 @@
             </el-breadcrumb>
         </div>
         <div class="content-main clearfix">
-            <div class="notice">
-                <div class="l">本演示后台数据为演示数据，可以随意操作，系统每5分钟重置一次</div>
-                <div class="r" v-if="resetVision">
-                    <label>重置倒计时：</label>
-                    <countdown class="count" endTime="" :callback="callback" endText="已经结束了"></countdown>
-                </div>
-            </div>
-            <div class="github">
-                <a target="_blank" href="https://github.com/iamdarcy">github地址</a>
-                <div class="l">请您在github给个star。 技术交流QQ群：824781955</div>
-            </div>
             <div class="header clearfix">
                 <el-card class="box-card card-red">
                     <router-link class="link-color" :to="{ path: '/dashboard/order' }">
