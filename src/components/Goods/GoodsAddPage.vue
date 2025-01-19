4eb00368d50cc5cb3fc846d8ae46807987fd5b82
zlaa <72907523+zlaazlaa@users.noreply.github.com>
2025-01-19 07:40:29 +0000

    Print time

--- src/components/Goods/GoodsAddPage.vue
+++ src/components/Goods/GoodsAddPage.vue
@@ -501,6 +501,16 @@ export default {
       });
     },
     async uploadToAzureBlob(SASString, containerName, file, fileName, callback) {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = now.getMonth() + 1;
+      const date = now.getDate();
+      const hours = now.getHours();
+      const minutes = now.getMinutes();
+      const seconds = now.getSeconds();
+
+      console.log(`当前时间：${year}-${month}-${date} ${hours}:${minutes}:${seconds}`);
+      
       const { BlobServiceClient } = require('@azure/storage-blob');
       const moment = require('moment'); 
 
